399a3bb2bf1cf2fefb818f007233709d521426cd
juzorey <josejosereyes75@gmail.com>
2023-09-27 10:25:06 -0400

    done

--- src/Home.js
+++ src/Home.js
@@ -6,22 +6,44 @@ import { BsArrowDown } from 'react-icons/bs'
 import Cabinet from './comp/project_cabinet.jsx'
 import SkillsBar from './comp/Skills-bar';
 import NavBar from './comp/NavBar';
+import { useTypewriter, Cursor } from 'react-simple-typewriter'
 
 function Home() {
+
+  const [text]= useTypewriter({
+    words:['Hello','Hola', 'Aloha'],
+    loop:{},
+    typspeed:200,
+    deleteSpeed:20
+  })
+
+  const downloadResume = ()=>{
+    fetch('Jose Reyes Resume 9-25-23.pdf').then(response=>{response.blob().then(blob=>{
+      const fileURL =window.URL.createObjectURL(blob) 
+      let alink = document.createElement('a')
+      alink.href = fileURL
+      alink.download = 'Jose Reyes Resume 9-25-23'
+      alink.click()
+    })})
+  }
   return (
   
     <div className='App' >
     <NavBar/>
     
       <header class="head">
-        <h1>Hello! I'm Jose Reyes</h1>
+        <h1>      
+          Aloha! I'm Jose Reyes
+
+        </h1>
+  
 
         <div className="right-head">
           <div className='right-head-text'>  <span className='right-head-text a'>Im a Software Engineer inspired to use the modern tools to surpass the current human limitation </span></div>
         
           <div class="link-container">
             <div class="contact-links">
-              <button class="btn"><FaFilePdf class="i-resume" /><a target="_blank" href="google.com btn">Resume</a></button>
+              <button class="btn" onClick={downloadResume}><FaFilePdf class="i-resume" /><a target="_blank" href="google.com btn">Resume</a></button>
               <button class="btn"><FaLinkedin class="i-linkedin" /><a target="_blank" href="google.com">LinkedIn</a></button>
               <button class="btn"><FaGithub class="i-github" /><a target="_blank" href="google.com">Github</a></button>
             </div>
--- src/comp/NavBar.jsx
+++ src/comp/NavBar.jsx
@@ -9,6 +9,25 @@ var element = document.querySelector('.skills-bar-container')
   element.scrollIntoView({behavior:"smooth", block: "end", inline:"nearest"});
 
 }
+function scroll2(){
+  var el = document.querySelector('.project-cabinet');
+  el.scrollIntoView({behavior:"smooth", block: "start", inline:"nearest"});
+}
+
+
+const downloadClick = ()=>{
+  fetch(`Jose Reyes Resume 9-25-23.pdf`).then(response=>{
+    response.blob().then(blob=>{
+      const fileURL = window.URL.createObjectURL(blob)
+      let alink = document.createElement('a')
+      alink.href =fileURL
+      alink.download = 'Jose Reyes Resume 9-25-23.pdf'
+      alink.click()
+    })
+  })
+}
+
+
   return(
 
 
@@ -17,9 +36,9 @@ var element = document.querySelector('.skills-bar-container')
       <div className="navbar-inner">
         <div className="nav-name">Jose Reyes</div>
         <div className="nav-links">
-          <Link to="/" className="nav-links-underline">RESUME</Link>
+          <Link to="/" className="nav-links-underline" onClick={downloadClick} value="download">RESUME</Link>
           <Link to="/" className="nav-links-underline" onClick={scroll}>SKILLS</Link>{/* scrolls down*/}
-          <Link to="/" className="nav-links-underline">PROJECTS</Link>
+          <Link to="/" className="nav-links-underline" onClick={scroll2}>PROJECTS</Link>
 
 
         </div>
